ec6f87ee3d64f15766565ae5e86b7477d21f724c
Adland Lee <adland@4mfd.com>
2016-01-26 14:01:05 -0800

    finish up recording view stub

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Adland Lee on 1/26/16.
+//  Copyright © 2016 Adland Lee. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func playSlowAudio(sender: UIButton) {
+        print("playSlowAudio")
+    }
+
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,9 +23,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func recordAudio(sender: UIButton) {
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide stop button
+        stopButton.hidden = true
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        // TODO: Show test  "recording in progress"
+        // TODO: Record user's voice
+        print("recordAudio")
+        recordButton.enabled = false
+        recordingLabel.hidden = false
+        stopButton.hidden = false
+    }
+
+    @IBAction func stopRecordAudio(sender: UIButton) {
+        print("stopRecordAudio")
+        recordButton.enabled = true
+        recordingLabel.hidden = true
+        stopButton.hidden = true
+    }
 }
 
