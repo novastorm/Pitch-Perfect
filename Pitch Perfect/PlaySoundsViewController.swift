1bc0291cc261c42229a2fdaaaf3e9a2370e467dc
Adland Lee <adland@4mfd.com>
2016-01-27 09:33:17 -0800

    add vader effect

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -76,6 +76,10 @@ class PlaySoundsViewController: UIViewController {
         audioPlayerNode.play()
     }
     
+    @IBAction func playVaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
     @IBAction func stopAudio(sender: UIButton) {
         audioPlayer.stop()
     }
